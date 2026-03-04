dc16d7c55427e1646b34060b1158a10e787f8c4e
joeajwas <163884130+joeajwas@users.noreply.github.com>
2026-03-04 18:40:35 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'blockbound-v2';
+const ASSETS = ['/', '/index.html', 'https://cdn.jsdelivr.net/npm/chart.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
